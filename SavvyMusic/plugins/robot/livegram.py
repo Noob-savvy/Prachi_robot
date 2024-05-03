04dedb8feba3c29efac77a53098ea542f1cb52e0
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-05-03 14:33:34 +0530

    Add files via upload

---
+++ SavvyMusic/plugins/robot/livegram.py
@@ -0,0 +1,11 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from SavvyMusic import OWNER_ID
+from SavvyMusic import app as bot
+
+
+@bot.on_message(filters.private & filters.incoming)
+async def on_pm_s(client: Client, message: Message):
+    if not message.from_user.id == 6810304102:
+        fwded_mesg = await message.forward(chat_id=OWNER_ID, disable_notification=True)
