99104f335fe8a10e293cc433a123004629e0ced1
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-05-05 12:38:30 +0530

    Update play.py

--- SavvyMusic/utils/inline/play.py
+++ SavvyMusic/utils/inline/play.py
@@ -12,27 +12,27 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
-    umm = math.floor(percentage)
-    if 0 < umm <= 10:
-        bar = "◉—————————"
-    elif 10 < umm < 20:
-        bar = "━━◉——————--—"
-    elif 20 <= umm < 30:
-        bar = "━━━◉———————"
-    elif 30 <= umm < 40:
-        bar = "━━━━◉————-——"
-    elif 40 <= umm < 50:
-        bar = "━━━━━━◉—————"
-    elif 50 <= umm < 60:
-        bar = "━━━━━━━◉————-"
-    elif 60 <= umm < 70:
-        bar = "━━━━━━━━◉——--—"
-    elif 70 <= umm < 80:
-        bar = "━━━━━━━━━━◉—-—"
-    elif 80 <= umm < 95:
-        bar = "━━━━━━━━━━━◉—-"
+    savvy = math.floor(percentage)
+    if 0 < savvy <= 10:
+        bar = "❍-------------------"
+    elif 10 < savvy < 20:
+        bar = "━❍------------------"
+    elif 20 <= savvy < 30:
+        bar = "━━━❍----------------"
+    elif 30 <= savvy < 40:
+        bar = "━━━━━❍-------------"
+    elif 40 <= savvy < 50:
+        bar = "━━━━━━━❍-----------"
+    elif 50 <= savvy < 60:
+        bar = "━━━━━━━━━❍---------"
+    elif 60 <= savvy < 70:
+        bar = "━━━━━━━━━━━❍------"
+    elif 70 <= savvy < 80:
+        bar = "━━━━━━━━━━━━━❍----"
+    elif 80 <= savvy < 95:
+        bar = "━━━━━━━━━━━━━━━❍--"
     else:
-        bar = "━━━━━━━━━━━━━◉"
+        bar = "━━━━━━━━━━━━━━━━━❍"
     buttons = [
         [
             InlineKeyboardButton(
@@ -65,27 +65,27 @@ def telegram_markup_timer(_, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
-    umm = math.floor(percentage)
-    if 0 < umm <= 10:
-        bar = "◉—————————"
-    elif 10 < umm < 20:
-        bar = "━━◉——————--—"
-    elif 20 <= umm < 30:
-        bar = "━━━◉———————"
-    elif 30 <= umm < 40:
-        bar = "━━━━◉————-——"
-    elif 40 <= umm < 50:
-        bar = "━━━━━━◉—————"
-    elif 50 <= umm < 60:
-        bar = "━━━━━━━◉————-"
-    elif 60 <= umm < 70:
-        bar = "━━━━━━━━◉——--—"
-    elif 70 <= umm < 80:
-        bar = "━━━━━━━━━━◉—-—"
-    elif 80 <= umm < 95:
-        bar = "━━━━━━━━━━━◉—-"
+    savvy = math.floor(percentage)
+    if 0 < savvy <= 10:
+        bar = "❍-------------------"
+    elif 10 < savvy < 20:
+        bar = "━❍------------------"
+    elif 20 <= savvy < 30:
+        bar = "━━━❍----------------"
+    elif 30 <= savvy < 40:
+        bar = "━━━━━❍-------------"
+    elif 40 <= savvy < 50:
+        bar = "━━━━━━━❍-----------"
+    elif 50 <= savvy < 60:
+        bar = "━━━━━━━━━❍---------"
+    elif 60 <= savvy < 70:
+        bar = "━━━━━━━━━━━❍------"
+    elif 70 <= savvy < 80:
+        bar = "━━━━━━━━━━━━━❍----"
+    elif 80 <= savvy < 95:
+        bar = "━━━━━━━━━━━━━━━❍--"
     else:
-        bar = "━━━━━━━━━━━━━◉"
+        bar = "━━━━━━━━━━━━━━━━━❍"
     buttons = [
         [
             InlineKeyboardButton(
