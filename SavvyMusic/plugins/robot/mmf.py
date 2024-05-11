2724e6d258cd06e5102dbbe693624fd454f9ca56
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-05-11 11:28:57 +0530

    Update mmf.py

--- SavvyMusic/plugins/robot/mmf.py
+++ SavvyMusic/plugins/robot/mmf.py
@@ -36,7 +36,7 @@ async def drawText(image_path, text):
     if os.name == "nt":
         fnt = "arial.ttf"
     else:
-        fnt = "./assets/default.ttf"
+        fnt = "assets/default.ttf"
 
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
 
